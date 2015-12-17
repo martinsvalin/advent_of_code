ef3d40311e6810a1ba473044572bf9c9dccbab9f
Martin Svalin <martin@lite.nu>
2015-12-17 22:52:59 +0100

    Variable prefix size, to solve bonus question.

--- 4/ruby/advent_coin.rb
+++ 4/ruby/advent_coin.rb
@@ -4,21 +4,23 @@ require 'digest'
 # when appended to the key and MD5 hashed, produces
 # a hexadecimal MD5 hash starting with five zeroes.
 class AdventCoin
-  def self.mine(key)
-    new(key).first_hit
+  def self.mine(key, prefix_size = 5)
+    new(key, prefix_size).first_hit
   end
 
-  def initialize(key)
+  def initialize(key, prefix_size)
     @key = key
+    @prefix = '0' * prefix_size
+    @size = prefix_size
   end
 
   def first_hit
-    attempts.find { |_, digest| digest[0, 5] == '00000' }.first
+    attempts.find { |_n, digest| digest[0, size] == prefix }.first
   end
 
   private
 
-  attr_reader :key
+  attr_reader :key, :prefix, :size
 
   def attempts
     all_numbers.map do |n|
@@ -44,4 +46,12 @@ check 'abcdef', 609_043
 check 'pqrstuv', 1_048_970
 
 MY_KEY = 'yzbqklnj'
-puts "The lowest possible number for #{MY_KEY} is #{AdventCoin.mine(MY_KEY)}."
+puts %(
+The lowest possible number for #{MY_KEY} with five zeros leading the digest \
+is #{AdventCoin.mine(MY_KEY)}.
+)
+
+puts %(
+The lowest possible number for #{MY_KEY} with six zeroes leading the digest \
+is #{AdventCoin.mine(MY_KEY, 6)}
+)
