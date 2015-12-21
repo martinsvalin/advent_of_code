90119fdcc9fc6672f96d33761647314f36e9377a
Martin Svalin <martin@lite.nu>
2015-12-22 00:30:05 +0100

    Refactor to use bitwise operations
    Still horrible performance, but most of the time is spent calculating the region

--- 6/ruby/lightshow.rb
+++ 6/ruby/lightshow.rb
@@ -1,3 +1,4 @@
+require 'matrix'
 ##
 # Turn on, off or toggle regions of a 1000x1000 grid of lights.
 # An instruction is in one of the following forms:
@@ -8,55 +9,112 @@ class Lights
   COORDINATES_REGEX = /(?<from_x>\d+),(?<from_y>\d+) through (?<to_x>\d+),(?<to_y>\d+)$/
 
   def self.count(instructions)
-    instructions.reduce([]) do |lights, instruction|
-      new(instruction).light_up(lights)
-    end.count
+    new(instructions).follow_instructions.count
   end
+
+  def initialize(instructions)
+    @grid = BitGrid.new size: 1000
+    @instructions = instructions.map { |i| Instruction.new(i) }
+  end
+
+  def follow_instructions
+    @grid = @instructions.reduce(@grid) do |grid, instruction|
+      instruction.perform_on(grid)
+    end
+  end
+
+  def count
+    @grid.count
+  end
+end
+
+##
+# Parses instructions into coordinates and commands to perform on the grid
+class Instruction
+  COORDINATES_REGEX = /(?<from_x>\d+),(?<from_y>\d+) through (?<to_x>\d+),(?<to_y>\d+)$/
 
   def initialize(instruction)
     @instruction = instruction
-    coordinates = COORDINATES_REGEX.match(instruction)
-    fail "Invalid instruction: #{instruction}" unless coordinates
-    @from_x = coordinates[:from_x].to_i
-    @from_y = coordinates[:from_y].to_i
-    @to_x = coordinates[:to_x].to_i
-    @to_y = coordinates[:to_y].to_i
   end
 
-  def light_up(lights)
-    case instruction
-    when /^turn on/ then turn_on(lights)
-    when /^turn off/ then turn_off(lights)
-    when /^toggle/ then toggle(lights)
+  def perform_on(grid)
+    case @instruction
+    when /^turn on/ then grid.turn_on(from, to)
+    when /^turn off/ then grid.turn_off(from, to)
+    when /^toggle/ then grid.toggle(from, to)
     end
   end
 
   private
 
-  attr_reader :instruction, :from_x, :from_y, :to_x, :to_y
-
-  def turn_on(lights)
-    lights + region
+  def from
+    [
+      coordinates[:from_x].to_i,
+      coordinates[:from_y].to_i
+    ]
   end
 
-  def turn_off(lights)
-    lights - region
+  def to
+    [
+      coordinates[:to_x].to_i,
+      coordinates[:to_y].to_i
+    ]
   end
 
-  def toggle(lights)
-    lights + region - (lights & region)
+  def coordinates
+    COORDINATES_REGEX.match @instruction
+  end
+end
+
+##
+# Represent a 2-dimensional grid of positions that can be ON or OFF, as a
+# 1-dimensional number of bits. For example, 0b1010 is a 2x2 grid where the
+# first column is OFF and the second column is ON. And 0b000111000 is a 3x3
+# grid where the centre row is ON.
+#
+# The grid can have regions turned on, off or toggled by providing coordinates
+# of the lower left and upper right corners of a region.
+class BitGrid
+  def initialize(size: 5)
+    @size = size
   end
 
-  def region
-    side_x.product(side_y)
+  def count
+    @grid.to_s(2).count('1')
   end
 
-  def side_x
-    (from_x..to_x).to_a
+  def turn_on(from, to)
+    @grid |= region(from, to)
   end
 
-  def side_y
-    (from_y..to_y).to_a
+  def turn_off(from, to)
+    @grid &= ~region(from, to)
+  end
+
+  def toggle(from, to)
+    @grid ^= region(from, to)
+  end
+
+  def to_s
+    @grid
+      .to_s(2)
+      .rjust((@size**2), '0')
+      .scan(/.{1,#{@size}}/)
+      .map(&:reverse)
+      .join("\n")
+  end
+
+  private
+
+  def region(from, to)
+    from_x, from_y = from
+    to_x, to_y = to
+
+    (from_x..to_x).reduce(0) do |total, row|
+      total + (from_y..to_y).reduce(0) do |line, col|
+        line + 2**(col + row * @size)
+      end
+    end
   end
 end
 
