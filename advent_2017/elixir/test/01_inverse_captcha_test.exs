887203de25a203d97348ab7c50234190e9edf640
Martin Svalin <martin@lite.nu>
2017-12-03 00:12:13 +0100

    Dec 1 problem 2 given test cases

--- advent_2017/elixir/test/01_inverse_captcha_test.exs
+++ advent_2017/elixir/test/01_inverse_captcha_test.exs
@@ -11,4 +11,14 @@ defmodule InverseCaptchaTest do
       assert sum_matching_next("91212129") == 9
     end
   end
+
+  describe "sum_matching_across" do
+    test "given test cases" do
+      assert sum_matching_across("1212") == 6
+      assert sum_matching_across("1221") == 0
+      assert sum_matching_across("123425") == 4
+      assert sum_matching_across("123123") == 12
+      assert sum_matching_across("12131415") == 4
+    end
+  end
 end
