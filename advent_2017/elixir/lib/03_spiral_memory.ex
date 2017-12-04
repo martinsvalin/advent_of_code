ddddfb5eb59a0071dd4895de550fead811e255b7
Martin Svalin <martin@lite.nu>
2017-12-05 00:48:45 +0100

    Revisit day 3, separate concerns of spiral and values

--- advent_2017/elixir/lib/03_spiral_memory.ex
+++ advent_2017/elixir/lib/03_spiral_memory.ex
@@ -1,51 +1,119 @@
-# 37 36 35 34 33 32 31
-# 38 17 16 15 14 13 30
-# 39 18  5  4  3 12 29
-# 40 19  6  1  2 11 28
-# 41 20  7  8  9 10 27
-# 42 21 22 23 24 25 26
-# 43 44 45 46 47 48 49
-
-
 defmodule SpiralMemory do
-  @next_direction %{right: :up, up: :left, left: :down, down: :right}
+  @moduledoc """
+  Spiral memory is essentially a linked list laid out in a spiral
+  """
 
-  def spiral(limit), do: make_spiral(%{}, {0,0}, 1, :down, limit)
+  @doc """
+  Gives the Manhattan distance from the start after following the spiral a number of steps
 
-  defp make_spiral(spiral, position, value, _direction, limit) when limit <= value do
-    Map.put_new(spiral, position, value)
+  ## Examples
+
+      iex> distance_from_start(1)
+      0
+      iex> distance_from_start(12)
+      3
+      iex> distance_from_start(23)
+      2
+      iex> distance_from_start(1024)
+      31
+  """
+  def distance_from_start(limit) do
+    [{x,y}] = Spiral.stream
+      |> Stream.drop(limit-1)
+      |> Enum.take(1)
+    abs(x) + abs(y)
   end
-  defp make_spiral(spiral, position, value, direction, limit) do
-    {new_position, new_direction} = turn_or_forward(spiral, position, direction)
-    new_spiral = Map.put_new(spiral, position, value)
 
-    make_spiral(
-      new_spiral, 
-      new_position, 
-      new_value(new_spiral, new_position), 
-      new_direction, 
-      limit
-    )
+  @doc """
+  Give values for spiral positions as a sum of values at previous positions
+
+  As we spiral outwards, set the value of each cell as the sum of values of
+  its neighbors including diagonals. The starting value is 1. This is returned
+  as a map %{coordinate => value}. The enumeration halts after a value that's
+  greater than a given limit.
+
+        147  142  133  122   59
+        304    5    4    2   57
+        330   10    1    1   54
+        351   11   23   25   26
+        362  747  806  -->  ...
+
+
+  ## Examples
+
+      iex> sum_of_neighbor_spiral(100)
+      %{
+                                       {1, 2}  => 122, {2, 2}  => 59,
+        {-1, 1}  => 5,  {0, 1}  => 4,  {1, 1}  => 2,   {2, 1}  => 57,
+        {-1, 0}  => 10, {0, 0}  => 1,  {1, 0}  => 1,   {2, 0}  => 54,
+        {-1, -1} => 11, {0, -1} => 23, {1, -1} => 25,  {2, -1} => 26
+      }
+  """
+  def sum_of_neighbor_spiral(limit) do
+    Enum.reduce_while(Spiral.stream, %{}, fn coord, map ->
+      case neighbors(map, coord) |> Enum.sum |> max(1) do
+        sum when sum >= limit -> {:halt, Map.put(map, coord, sum)}
+        sum                   -> {:cont, Map.put(map, coord, sum)}
+      end
+    end)
   end
 
-  defp new_value(spiral, position) do
-    Map.get(spiral, up(left(position)), 0)   + Map.get(spiral, up(position), 0)   + Map.get(spiral, up(right(position)), 0) +
-    Map.get(spiral, left(position), 0)                                            + Map.get(spiral, right(position), 0) +
-    Map.get(spiral, down(left(position)), 0) + Map.get(spiral, down(position), 0) + Map.get(spiral, down(right(position)), 0)
+  defp neighbors(map, {x,y}) do
+    for xx <- x-1..x+1,
+        yy <- y-1..y+1,
+        map[{xx, yy}]
+    do
+      map[{xx, yy}]
+    end
+  end
+end
+
+defmodule Spiral do
+  @moduledoc """
+  Generate cartesian coordinates in a spiral
+
+  The first cell is in the center, going left, up, right, down etc:
+
+      17  16  15  14  13
+      18  5   4   3   12
+      19  6   1   2   11
+      20  7   8   9   10
+      21  22  23 --> ...
+  """
+
+  @doc """
+  Generate spiral coordinates as a stream, starting from {0,0}
+  """
+  def stream do
+    Stream.unfold({MapSet.new, {0,0}, :south}, &unfold_spiral/1)
   end
 
-  defp turn_or_forward(spiral, position, direction) do
-    case spiral[turn(position, direction)] do
-      nil -> {turn(position, direction), @next_direction[direction]}
-      _   -> {forward(position, direction), direction}
+  defp unfold_spiral({set, coordinate, direction}) do
+    new_set = MapSet.put(set, coordinate)
+    {next_coordinate, next_direction} = next(new_set, coordinate, direction)
+
+    {coordinate, {new_set, next_coordinate, next_direction}}
+  end
+
+  @rotate %{south: :east, east: :north, north: :west, west: :south}
+
+  defp next(set, coordinate, direction) do
+    if MapSet.member?(set, left(coordinate, direction)) do
+      {forward(coordinate, direction), direction}
+    else
+      {left(coordinate, direction), @rotate[direction]}
     end
   end
 
-  defp forward(position, direction), do: apply(__MODULE__, direction, [position])
-  defp turn(position, direction), do: apply(__MODULE__, @next_direction[direction], [position])
+  defp forward(coordinate, direction), do: apply(__MODULE__, direction, [coordinate])
+  defp left(coordinate, direction), do: apply(__MODULE__, @rotate[direction], [coordinate])
 
-  def right({x,y}), do: {x + 1, y}
-  def left({x,y}),  do: {x - 1, y}
-  def up({x,y}),    do: {x, y + 1}
-  def down({x,y}),  do: {x, y - 1}
+  @doc false
+  def south({x,y}), do: {x, y-1}
+  @doc false
+  def north({x,y}), do: {x, y+1}
+  @doc false
+  def east({x,y}),  do: {x+1, y}
+  @doc false
+  def west({x,y}),  do: {x-1, y}
 end
--- advent_2017/elixir/test/03_spiral_memory_test.exs
+++ advent_2017/elixir/test/03_spiral_memory_test.exs
@@ -1,24 +1,5 @@
 defmodule SpiralMemoryTest do
   use ExUnit.Case
-
-  describe "spiral" do
-    test "given test cases" do
-      values = [
-        147,  142,  133,  122,   59,
-        304,    5,    4,    2,   57,
-        330,   10,    1,    1,   54,
-        351,   11,   23,   25,   26,
-        362,  747,  806,
-      ]
-      keys = [
-        {-2,  2}, {-1,  2}, {0,  2}, {1,  2}, {2,  2},
-        {-2,  1}, {-1,  1}, {0,  1}, {1,  1}, {2,  1},
-        {-2,  0}, {-1,  0}, {0,  0}, {1,  0}, {2,  0},
-        {-2, -1}, {-1, -1}, {0, -1}, {1, -1}, {2, -1},
-        {-2, -2}, {-1, -2}, {0, -2}, {1, -2}, {2, -2}
-      ]
-
-      assert SpiralMemory.spiral(806) == Map.new(Enum.zip keys, values)
-    end
-  end
+  import SpiralMemory
+  doctest SpiralMemory
 end
