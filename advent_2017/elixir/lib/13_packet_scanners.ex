dfa2b33e6428a3e53b8ee51cae0fdac01f50a9a0
Martin Svalin <martin@lite.nu>
2017-12-13 23:30:28 +0100

    Refactor day 13, big cleanup

--- advent_2017/elixir/lib/13_packet_scanners.ex
+++ advent_2017/elixir/lib/13_packet_scanners.ex
@@ -16,69 +16,29 @@ defmodule PacketScanners do
   """
   @spec total_severity(String.t()) :: non_neg_integer()
   def total_severity(input, delay \\ 0) do
-    {scanners, max_depth} = parse(input)
-
-    total_severity(scanners, max_depth, delay)
-  end
-
-  defp total_severity(scanners, max_depth, delay) do
-    severities(scanners, max_depth, delay)
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.sum()
+    for {depth, range} <- scanners(input), caught_at?(range, depth + delay) do
+      depth * range
+    end
+    |> Enum.sum
   end
 
   @doc """
   Find the best delay when you can pass through the scanners without being caught
   """
-  def sneaky_time(input) do
-    {scanners, max_depth} = parse(input)
+  def sneaky_time(input), do: sneaky_time(scanners(input), 0)
 
-    sneaky_time(scanners, max_depth, 0)
-  end
-
-  defp sneaky_time(scanners, max_depth, delay) do
-    undetected =
-      severities(scanners, max_depth, delay)
-      |> Enum.all?(&(elem(&1, 0) == :undetected))
-
-    case undetected do
-      true -> delay
-      _ -> sneaky_time(scanners, max_depth, delay + 1)
+  defp sneaky_time(scanners, delay) do
+    caught? = Enum.any?(scanners, fn {depth, range} -> caught_at?(range, depth + delay) end)
+    case caught? do
+      true -> sneaky_time(scanners, delay + 1)
+      false -> delay
     end
   end
 
-  @doc """
-  The severity of getting caught for each step when starting at a given delay
-  """
-  @spec severities(map(), non_neg_integer(), non_neg_integer()) :: [
-          {:caught | :undetected, non_neg_integer()}
-        ]
-  def severities(scanners, max_depth, delay) do
-    0..max_depth
-    |> Enum.zip(delay..(max_depth + delay))
-    |> Enum.map(&severity(scanners, &1))
-  end
+  defp caught_at?(range, time), do: rem(time, (range - 1) * 2) == 0
 
-  @doc """
-  The severity of getting caught at a given depth and time. Zero if we're not caught at that time.
-  """
-  @spec severity(map(), {non_neg_integer(), non_neg_integer()}) ::
-          {:caught | :undetected, non_neg_integer()}
-  def severity(scanners, {depth, time}) do
-    range = scanners[depth]
-
-    if caught?(range, time), do: {:caught, range * depth}, else: {:undetected, 0}
-  end
-
-  defp caught?(nil, _time), do: false
-  defp caught?(range, time), do: rem(time, (range - 1) * 2) == 0
-
-  defp parse(string) do
-    scanners =
-      Regex.scan(~r/(\d+): (\d+)/, string)
-      |> Enum.map(fn [_, depth, range] -> {String.to_integer(depth), String.to_integer(range)} end)
-      |> Map.new()
-
-    {scanners, Map.keys(scanners) |> Enum.max()}
+  defp scanners(string) do
+    Regex.scan(~r/(\d+): (\d+)/, string)
+    |> Map.new(fn [_, depth, range] -> {String.to_integer(depth), String.to_integer(range)} end)
   end
 end
