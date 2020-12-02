ac577a9bcf719145e22e3e4a96fba77dabda88bd
Martin Svalin <martin@lite.nu>
2020-12-02 23:07:17 +0100

    Day 2, Password philosophy, character in one of the positions

--- 2020/elixir/lib/02_password_philosophy.ex
+++ 2020/elixir/lib/02_password_philosophy.ex
@@ -1,9 +1,13 @@
 defmodule PasswordPhilosophy do
-  def count_valid_passwords(lines) do
+  def count_valid_passwords_by_frequency(lines) do
     lines
     |> parse
     |> character_frequency
-    |> Enum.count(&valid_for_part1?/1)
+    |> Enum.count(&valid_frequencies?/1)
+  end
+
+  def count_valid_passwords_by_position(lines) do
+    lines |> parse |> Enum.count(&valid_positions?/1)
   end
 
   defp parse(lines) when is_list(lines), do: Enum.map(lines, &parse/1)
@@ -12,7 +16,7 @@ defmodule PasswordPhilosophy do
     [_full, first, last, <<char::utf8>>, password] = Regex.run(~r/(\d+)-(\d+) (.): (.+)/, line)
 
     %{
-      password: password,
+      password: to_charlist(password),
       character: char,
       range: String.to_integer(first)..String.to_integer(last)
     }
@@ -21,10 +25,15 @@ defmodule PasswordPhilosophy do
   defp character_frequency(list) when is_list(list), do: Enum.map(list, &character_frequency/1)
 
   defp character_frequency(%{password: password} = map) do
-    Map.put_new(map, :frequencies, password |> to_charlist() |> Enum.frequencies())
+    Map.put_new(map, :frequencies, password |> Enum.frequencies())
   end
 
-  defp valid_for_part1?(%{character: char, range: range, frequencies: frequencies}) do
+  defp valid_frequencies?(%{character: char, range: range, frequencies: frequencies}) do
     frequencies[char] in range
+  end
+
+  defp valid_positions?(%{character: char, range: first..second, password: password}) do
+    [Enum.at(password, first - 1), Enum.at(password, second - 1)]
+    |> Enum.count(&(&1 == char)) == 1
   end
 end
--- 2020/elixir/test/02_password_philosophy_test.exs
+++ 2020/elixir/test/02_password_philosophy_test.exs
@@ -1,18 +1,37 @@
 defmodule PasswordPhilosophyTest do
   use ExUnit.Case
 
-  describe "count_valid_passwords/1" do
-    test "with one valid line" do
-      assert PasswordPhilosophy.count_valid_passwords(["1-3 a: abc"]) == 1
+  describe "count_valid_passwords_by_frequency/1" do
+    test "where we want 1-3 `a` characters and have a valid password" do
+      assert PasswordPhilosophy.count_valid_passwords_by_frequency(["1-3 a: abc"]) == 1
     end
 
-    test "with one invalid line" do
-      assert PasswordPhilosophy.count_valid_passwords(["1-3 a: bce"]) == 0
+    test "where we want 1-3 `d` characters and have an invalid password" do
+      assert PasswordPhilosophy.count_valid_passwords_by_frequency(["1-3 d: abc"]) == 0
     end
 
     test "given example" do
       lines = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
-      assert PasswordPhilosophy.count_valid_passwords(lines) == 2
+      assert PasswordPhilosophy.count_valid_passwords_by_frequency(lines) == 2
+    end
+  end
+
+  describe "count_valid_passwords_by_position" do
+    test "where we want an `a` character at positions 1 xor 3, with a valid password" do
+      assert PasswordPhilosophy.count_valid_passwords_by_position(["1-3 a: abc"]) == 1
+    end
+
+    test "where we want an `a` character at positions 1 xor 3, with no `a` in the password" do
+      assert PasswordPhilosophy.count_valid_passwords_by_position(["1-3 a: dbc"]) == 0
+    end
+
+    test "where we want an `a` character at positions 1 xor 3, with `a` in both positions" do
+      assert PasswordPhilosophy.count_valid_passwords_by_position(["1-3 a: aba"]) == 0
+    end
+
+    test "given example" do
+      lines = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+      assert PasswordPhilosophy.count_valid_passwords_by_position(lines) == 1
     end
   end
 end
