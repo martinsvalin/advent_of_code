9db1f54f9c54d0de42534e692f966fb2cb0c65d8
Martin Svalin <martin@lite.nu>
2020-12-21 09:12:35 +0100

    Day 17, Conway Cubes, buy now and get 1 dimension extra for free!

--- 2020/elixir/lib/17_conway_cubes.ex
+++ 2020/elixir/lib/17_conway_cubes.ex
@@ -1,44 +1,59 @@
 defmodule ConwayCubes do
-  def run(input, ticks) do
+  def run(input, ticks, dimensions \\ 3) do
     start_cubes = parse(input)
 
     Enum.reduce(1..ticks, start_cubes, fn _, cubes ->
-      tick(cubes)
+      tick(cubes, dimensions)
     end)
   end
 
-  def tick(cubes) do
-    still_active = cubes |> survivors()
-    activated = cubes |> inactive_neighbours() |> activate(cubes)
+  def tick(cubes, dimensions) do
+    still_active = cubes |> survivors(dimensions)
+
+    activated =
+      cubes
+      |> inactive_neighbours(dimensions)
+      |> activate(cubes, dimensions)
+
     MapSet.union(still_active, activated)
   end
 
-  def survivors(cubes) do
-    for cube <- cubes, survives?(cubes, cube), into: MapSet.new(), do: cube
+  def survivors(cubes, dimensions) do
+    for cube <- cubes, survives?(cubes, cube, dimensions), into: MapSet.new(), do: cube
   end
 
-  def survives?(cubes, cube) do
-    MapSet.size(MapSet.intersection(cubes, neighbours(cube))) in 2..3
+  def survives?(cubes, cube, dimensions) do
+    MapSet.size(MapSet.intersection(cubes, neighbours(cube, dimensions))) in 2..3
   end
 
-  def neighbours({cube_x, cube_y, cube_z}) do
+  def neighbours({cube_x, cube_y, cube_z, cube_w}, 3) do
     for x <- -1..1,
         y <- -1..1,
         z <- -1..1,
-        {cube_x + x, cube_y + y, cube_z + z} != {cube_x, cube_y, cube_z},
+        {cube_x + x, cube_y + y, cube_z + z, cube_w} != {cube_x, cube_y, cube_z, cube_w},
         into: MapSet.new(),
-        do: {cube_x + x, cube_y + y, cube_z + z}
+        do: {cube_x + x, cube_y + y, cube_z + z, cube_w}
   end
 
-  def inactive_neighbours(cubes) do
+  def neighbours({cube_x, cube_y, cube_z, cube_w}, 4) do
+    for x <- -1..1,
+        y <- -1..1,
+        z <- -1..1,
+        w <- -1..1,
+        {cube_x + x, cube_y + y, cube_z + z, cube_w + w} != {cube_x, cube_y, cube_z, cube_w},
+        into: MapSet.new(),
+        do: {cube_x + x, cube_y + y, cube_z + z, cube_w + w}
+  end
+
+  def inactive_neighbours(cubes, dimensions) do
     Enum.reduce(cubes, MapSet.new(), fn cube, inactive ->
-      MapSet.difference(neighbours(cube), cubes) |> MapSet.union(inactive)
+      MapSet.difference(neighbours(cube, dimensions), cubes) |> MapSet.union(inactive)
     end)
   end
 
-  def activate(inactive, active) do
+  def activate(inactive, active, dimensions) do
     for cube <- inactive,
-        neighbours = neighbours(cube),
+        neighbours = neighbours(cube, dimensions),
         MapSet.size(MapSet.intersection(neighbours, active)) == 3,
         into: MapSet.new(),
         do: cube
@@ -46,26 +61,26 @@ defmodule ConwayCubes do
 
   ## Parsing and printing
 
-  def parse(input, coordinate \\ {0, 0, 0}, out \\ MapSet.new())
+  def parse(input, coordinate \\ {0, 0, 0, 0}, out \\ MapSet.new())
 
   def parse("", _, out), do: out
 
-  def parse("." <> input, {x, y, z}, out), do: parse(input, {x + 1, y, z}, out)
+  def parse("." <> input, {x, y, z, w}, out), do: parse(input, {x + 1, y, z, w}, out)
 
-  def parse("#" <> input, {x, y, z}, out) do
-    parse(input, {x + 1, y, z}, MapSet.put(out, {x, y, z}))
+  def parse("#" <> input, {x, y, z, w}, out) do
+    parse(input, {x + 1, y, z, w}, MapSet.put(out, {x, y, z, w}))
   end
 
-  def parse("\n" <> input, {_x, y, z}, out), do: parse(input, {0, y + 1, z}, out)
+  def parse("\n" <> input, {_x, y, z, w}, out), do: parse(input, {0, y + 1, z, w}, out)
 
-  def to_string(cubes, z) do
-    {{min_x, min_y, _}, {max_x, max_y, _}} = minmax(cubes)
-    plane = Enum.filter(cubes, &match?({_, _, ^z}, &1)) |> MapSet.new()
+  def to_string(cubes, z, w \\ 0) do
+    {{min_x, min_y, _, _}, {max_x, max_y, _, _}} = minmax(cubes)
+    plane = Enum.filter(cubes, &match?({_, _, ^z, ^w}, &1)) |> MapSet.new()
 
     lines =
       for y <- min_y..max_y do
         for x <- min_x..max_x do
-          if {x, y, z} in plane, do: ?#, else: ?.
+          if {x, y, z, w} in plane, do: ?#, else: ?.
         end
       end
 
@@ -78,11 +93,11 @@ defmodule ConwayCubes do
 
   defp minmax([], minmax), do: minmax
 
-  defp minmax([{x, y, z} | tail], {{min_x, min_y, min_z}, {max_x, max_y, max_z}}) do
+  defp minmax([{x, y, z, w} | tail], {{min_x, min_y, min_z, min_w}, {max_x, max_y, max_z, max_w}}) do
     minmax(
       tail,
-      {{min(x, min_x), min(y, min_y), min(z, min_z)},
-       {max(x, max_x), max(y, max_y), max(z, max_z)}}
+      {{min(x, min_x), min(y, min_y), min(z, min_z), min(w, min_w)},
+       {max(x, max_x), max(y, max_y), max(z, max_z), max(w, max_w)}}
     )
   end
 end
--- 2020/elixir/test/17_conway_cubes_test.exs
+++ 2020/elixir/test/17_conway_cubes_test.exs
@@ -147,16 +147,24 @@ defmodule ConwayCubesTest do
     end
   end
 
+  describe "run/3 lets you run with 4 dimensions" do
+    @tag :slow
+    test "example 6 cycles" do
+      cubes = ConwayCubes.run(@example, 6, 4)
+      assert MapSet.size(cubes) == 848
+    end
+  end
+
   describe "parse/1" do
-    test "gives a set of 3d coordinates of active cubes" do
+    test "gives a set of 4d coordinates of active cubes" do
       assert ConwayCubes.parse(@example) ==
-               MapSet.new([{1, 0, 0}, {2, 1, 0}, {0, 2, 0}, {1, 2, 0}, {2, 2, 0}])
+               MapSet.new([{1, 0, 0, 0}, {2, 1, 0, 0}, {0, 2, 0, 0}, {1, 2, 0, 0}, {2, 2, 0, 0}])
     end
   end
 
   describe "to_string/2" do
     test "prints the 2d grid at the given z-index" do
-      grid = MapSet.new([{1, 0, 0}, {2, 1, 0}, {0, 2, 0}, {1, 2, 0}, {2, 2, 0}])
+      grid = MapSet.new([{1, 0, 0, 0}, {2, 1, 0, 0}, {0, 2, 0, 0}, {1, 2, 0, 0}, {2, 2, 0, 0}])
 
       assert ConwayCubes.to_string(grid, 0) == @example
     end
