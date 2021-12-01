d84c58aa33a7f89c10a76b478e4124aff226d40c
Martin Svalin <martin@lite.nu>
2021-12-01 23:53:27 +0100

    2021, day 1, learning typescript

---
+++ 2021/typescript/src/01_sonar_sweep.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from "fs"
+
+let input = readFileSync("../input/01")
+  .toString()
+  .split("\n")
+  .map((n) => parseInt(n))
+input.pop()
+
+// Example:
+// > sliding_window([1,2,3,4,5,6], 3)
+// [[1,2,3], [2,3,4], [3,4,5], [4,5,6]]
+function sliding_window(list: any[], size: number): any[][] {
+  let result = []
+  for (var i = 0; i <= list.length - size; i++) {
+    result.push(list.slice(i, i + size))
+  }
+  return result
+}
+
+function count_increases(list: number[]): number {
+  return sliding_window(list, 2).filter(([left, right]) => left < right).length
+}
+
+// First problem:
+// Count how many times the depth increases between two consecutive measurements?
+
+console.log("part 1", count_increases(input))
+
+// Second problem:
+// Consider groups of three. Sum these groups. How many times does depth increase then?
+
+function sum(list: number[]): number {
+  return list.reduce((a, b) => a + b)
+}
+
+let groups = sliding_window(input, 3).map(sum)
+console.log("part 2", count_increases(groups))
