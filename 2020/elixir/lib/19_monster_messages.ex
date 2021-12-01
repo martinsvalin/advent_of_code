5eabcf585c1a1c9797af40bb6abaedc11c4d28ba
Martin Svalin <martin@lite.nu>
2021-12-01 22:36:29 +0100

    WIP left-over stuff from 2020

--- 2020/elixir/lib/19_monster_messages.ex
+++ 2020/elixir/lib/19_monster_messages.ex
@@ -1,7 +1,20 @@
 defmodule MonsterMessages do
   import Advent2020, only: [lines: 1]
 
+  def modify_rules(%{rules: rules} = map) do
+    %{map | rules: modify_rules(rules)}
+  end
+
+  def modify_rules(rules) do
+    rules
+    |> Map.put(8, [[42, 8], [42]])
+    |> Map.put(11, [[42, 11, 31], [42, 31]])
+  end
+
+  ## Matching
+
   def match?(message, rules) do
+    IO.inspect({message, rules}, label: "Start matching")
     {:ok, ""} == match?(message, rules, rules[0])
   end
 
@@ -9,34 +22,69 @@ defmodule MonsterMessages do
     match?(message, rules, rule)
   end
 
-  def match?(<<rule::bytes-1>> <> rest, _rules, rule) when is_binary(rule) do
-    {:ok, rest}
-  end
+  def match?("a" <> rest, _rules, "a"), do: {:ok, rest}
+  def match?("b" <> rest, _rules, "b"), do: {:ok, rest}
 
-  def match?(message, _rules, rule)
-      when is_binary(rule) and is_binary(message),
-      do: :error
+  def match?(message, _rules, rule) when is_binary(rule) and is_binary(message) do
+    {:error, message}
+  end
 
   def match?(message, rules, [alt1, alt2])
       when is_list(alt1) and is_list(alt2) and is_binary(message) do
+    IO.inspect({message, {alt1, alt2}}, label: "Rule has 2 alternatives", charlists: :as_lists)
+
     case match?(message, rules, alt1) do
-      :error -> match?(message, rules, alt2)
-      {:ok, rest} -> {:ok, rest}
+      {:error, _} ->
+        IO.puts(
+          "Alt 1 failed: #{inspect(alt1, charlists: :as_lists)}. Will try Alt 2: #{
+            inspect(alt2, charlists: :as_lists)
+          }"
+        )
+
+        match?(message, rules, alt2)
+
+      {:ok, rest} ->
+        IO.puts("Alt 1 succeed: #{inspect(alt1, charlists: :as_lists)}")
+        {:ok, rest}
     end
+  end
+
+  def match?(message, rules, [binary | _] = binaries) when is_binary(binary) do
+    IO.puts("Matching #{length(binaries)} binary alternatives.")
+
+    Enum.find_value(
+      binaries,
+      {:error, message},
+      fn binary ->
+        case match?(message, rules, binary) do
+          {:ok, rest} -> {:ok, rest}
+          {:error, _} -> false
+        end
+      end
+    )
   end
 
   def match?(message, rules, [list]) when is_list(list), do: match?(message, rules, list)
 
-  def match?(message, rules, list) when is_binary(message) do
-    Enum.reduce_while(list, message, fn rule, remaining_message ->
-      case match?(remaining_message, rules, rules[rule]) do
-        :error -> {:halt, :error}
-        {:ok, rest} -> {:cont, {:ok, rest}}
-      end
-    end)
+  def match?(message, _rules, []) when is_binary(message) do
+    {:ok, message}
   end
 
-  # Parsing
+  def match?(message, rules, [rule | list]) when is_binary(message) do
+    IO.inspect({message, rule}, label: "Matching rule #{rule}")
+
+    case match?(message, rules, rules[rule]) do
+      {:error, msg} ->
+        IO.puts("Rule #{rule} failed. Won't try rest: #{inspect(list, charlists: :as_lists)}")
+        {:error, msg}
+
+      {:ok, rest} ->
+        IO.puts("Rule #{rule} succeed. Will try rest: #{inspect(list, charlists: :as_lists)}")
+        match?(rest, rules, list)
+    end
+  end
+
+  ## Parsing
 
   def parse(input) do
     [raw_rules, raw_messages] = String.split(input, "\n\n")
--- 2020/elixir/test/19_monster_messages_test.exs
+++ 2020/elixir/test/19_monster_messages_test.exs
@@ -17,6 +17,56 @@ defmodule MonsterMessagesTest do
   aaaabbb
   """
 
+  @bigger_example """
+  42: 9 14 | 10 1
+  9: 14 27 | 1 26
+  10: 23 14 | 28 1
+  1: "a"
+  11: 42 31
+  5: 1 14 | 15 1
+  19: 14 1 | 14 14
+  12: 24 14 | 19 1
+  16: 15 1 | 14 14
+  31: 14 17 | 1 13
+  6: 14 14 | 1 14
+  2: 1 24 | 14 4
+  0: 8 11
+  13: 14 3 | 1 12
+  15: 1 | 14
+  17: 14 2 | 1 7
+  23: 25 1 | 22 14
+  28: 16 1
+  4: 1 1
+  20: 14 14 | 1 15
+  3: 5 14 | 16 1
+  27: 1 6 | 14 18
+  14: "b"
+  21: 14 1 | 1 14
+  25: 1 1 | 1 14
+  22: 14 14
+  8: 42
+  26: 14 22 | 1 20
+  18: 15 15
+  7: 14 5 | 1 21
+  24: 14 1
+
+  abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+  bbabbbbaabaabba
+  babbbbaabbbbbabbbbbbaabaaabaaa
+  aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+  bbbbbbbaaaabbbbaaabbabaaa
+  bbbababbbbaaaaaaaabbababaaababaabab
+  ababaaaaaabaaab
+  ababaaaaabbbaba
+  baabbaaaabbaaaababbaababb
+  abbbbabbbbaaaababbbbbbaaaababb
+  aaaaabbaabaaaaababaa
+  aaaabbaaaabbaaa
+  aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+  babaaabbbaaabaababbaabababaaab
+  aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+  """
+
   describe "match?/2" do
     @rules MM.parse(@example).rules
     @messages MM.parse(@example).messages
@@ -26,6 +76,23 @@ defmodule MonsterMessagesTest do
       test "with message: #{message}" do
         assert MM.match?(unquote(message), @rules) == unquote(expected)
       end
+    end
+
+    test "bigger example matches 3 messages" do
+      parsed = MM.parse(@bigger_example)
+      assert Enum.count(parsed.messages, &MM.match?(&1, parsed.rules)) == 3
+    end
+
+    @tag :skip
+    test "if we modify the rules, 12 messages in the bigger example match" do
+      parsed = MM.parse(@bigger_example)
+      rules = MM.modify_rules(parsed.rules)
+      assert Enum.count(parsed.messages, &MM.match?(&1, rules)) == 12
+    end
+
+    test "a short message that should match with modified rules" do
+      parsed = @bigger_example |> MM.parse() |> MM.modify_rules()
+      assert MM.match?("aaaabbaaaabbaaa", parsed.rules)
     end
   end
 
