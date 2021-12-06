c686997bacc8c3886e40338da3b15d6540cd0adb
Martin Svalin <martin@lite.nu>
2021-12-06 23:25:14 +0100

    2021, day 5, adding the diagonal lines

--- 2021/typescript/src/05_hydrothermal_venture.ts
+++ 2021/typescript/src/05_hydrothermal_venture.ts
@@ -8,7 +8,7 @@ class Coordinate {
     return `${this.x},${this.y}`
   }
 }
-type direction = "horizontal" | "vertical" | "slanted"
+type direction = "horizontal" | "vertical" | "diagonal"
 type ventLine = {
   direction: direction
   from: Coordinate
@@ -16,6 +16,9 @@ type ventLine = {
   coordinates: Coordinate[]
 }
 
+// Parse input into vent lines, with a direction, from and to coordinates
+// and an empty set of the full coordinates the line passes through
+// which will be filled in shortly.
 let ventLines: ventLine[] = rawInput.map((line) => {
   let [[x1, y1], [x2, y2]] = line
     .split(" -> ", 2)
@@ -31,14 +34,11 @@ let ventLines: ventLine[] = rawInput.map((line) => {
   } else if (left.x < right.x && left.y === right.y) {
     return { direction: "vertical", from: left, to: right, coordinates: [] }
   } else {
-    return { direction: "slanted", from: left, to: right, coordinates: [] }
+    return { direction: "diagonal", from: left, to: right, coordinates: [] }
   }
 })
 
-console.log("horizontal", ventLines.filter(({ direction }) => direction === "horizontal").length)
-console.log("vertical", ventLines.filter(({ direction }) => direction === "vertical").length)
-console.log("slanted", ventLines.filter(({ direction }) => direction === "slanted").length)
-
+// Fill in each coordinate a line passes through, ignoring diagonal lines.
 ventLines.forEach((ventLine) => {
   switch (ventLine.direction) {
     case "vertical":
@@ -51,19 +51,58 @@ ventLines.forEach((ventLine) => {
         ventLine.coordinates.push(new Coordinate(ventLine.from.x, y))
       }
       break
-    case "slanted":
+    case "diagonal":
       break
   }
 })
 
-let coordinateHistogram = ventLines.reduce((acc: Map<string, number>, { coordinates }) => {
-  coordinates.forEach((c) => acc.set(c.toString(), (acc.get(c.toString()) || 0) + 1))
-  return acc
-}, new Map())
+// With each vent line's full set of coordinates, we can now create a histogram
+// counting the vent lines that pass through each coordinate.
+// For comparable keys, we use the string representation of Coordinate as key
+function createHistogram(ventLines: ventLine[]): Map<string, number> {
+  return ventLines.reduce((acc, { coordinates }) => {
+    coordinates.forEach((c) => acc.set(c.toString(), (acc.get(c.toString()) || 0) + 1))
+    return acc
+  }, new Map<string, number>())
+}
 
+// Collect the dangerous coordinates, i.e. the ones with more than one vent line
 let dangerousCoordinates: string[] = []
-coordinateHistogram.forEach((val, key) => {
+createHistogram(ventLines).forEach((val, key) => {
   if (val > 1) dangerousCoordinates.push(key)
 })
 
 console.log("part1", dangerousCoordinates.length)
+
+const increment = (n: number) => n + 1
+const decrement = (n: number) => n - 1
+
+// Now it's time to set the coordinates where diagonal vent lines pass through.
+ventLines
+  .filter(({ direction }) => direction === "diagonal")
+  .forEach((ventLine) => {
+    // Determine the direction to move x and y, to go from `from` to `to`.
+    // We'll use this to step closer when iterating.
+    let horizontalMove = ventLine.from.x < ventLine.to.x ? increment : decrement
+    let verticalMove = ventLine.from.y < ventLine.to.y ? increment : decrement
+    let [x, y] = [ventLine.from.x, ventLine.from.y]
+
+    ventLine.coordinates.push(ventLine.from)
+
+    // Since diagonal lines are always 45°, it doesn't matter stop at the final x or y.
+    // We'll reach both at the same time.
+    while (x != ventLine.to.x) {
+      x = horizontalMove(x)
+      y = verticalMove(y)
+      ventLine.coordinates.push(new Coordinate(x, y))
+    }
+  })
+
+// Collect the dangerous coordinates again, i.e. the ones with more than one vent line
+// Since the histogram is created for all lines (vertical, horizontal and diagonal),
+// we need to clear the dangerous coordinates we had before, or they'll be double-counted
+dangerousCoordinates = []
+createHistogram(ventLines).forEach((val, key) => {
+  if (val > 1) dangerousCoordinates.push(key)
+})
+console.log("part2", dangerousCoordinates.length)
