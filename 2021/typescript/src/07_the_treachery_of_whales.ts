636f8e572624ac8e4da485af2009ea97a223b3a8
Martin Svalin <martin@lite.nu>
2021-12-09 21:14:33 +0100

    2021, day 7, see it could be much simpler

--- 2021/typescript/src/07_the_treachery_of_whales.ts
+++ 2021/typescript/src/07_the_treachery_of_whales.ts
@@ -9,46 +9,18 @@ let crabs: number[] = readFileSync("../input/07")
 // I've got crabs at position 0, but otherwise we'd have to normalize them like
 // crabs.map((n) => n - crabs[0])
 
-type Zipper = {
-  left: number[]
-  right: number[]
-  sumLeft: number
-  sumRight: number
-}
 type costFunction = { (n: number, position: number): number }
 
 // Part 1, crabs move 1 position at the cost of 1 unit of fuel
 let partOneCostFunction: costFunction = (n: number, position: number) => Math.abs(n - position)
 
-// Strategy is to move across all the positions and move crabs at those positions
-// over from our right-side list to our left-side list, while updating the sum
-// fuel cost of all crabs to our left (increasing) and to our right (decreasing)
+// Apply the cost function for each crab at each position, building up a list of
+// sums of fuel costs that we can find the minimum in.
 function calculateCostsForAllPositions(cost: costFunction): number[] {
-  let position = 0
   let sums: number[] = []
-  let zipper: Zipper = {
-    left: [],
-    right: [...crabs],
-    sumLeft: 0,
-    sumRight: crabs.reduce((sum, n) => sum + cost(n, position), 0),
-  }
 
-  while (zipper.right.length) {
-    // move over crabs at this position to the left
-    while (zipper.right[0] === position) {
-      let mover = zipper.right.shift()
-      if (mover !== undefined) {
-        zipper.left.unshift(mover)
-      }
-    }
-
-    // Update the sum
-    zipper.sumLeft = zipper.left.reduce((sum, n) => sum + cost(n, position), 0)
-    zipper.sumRight = zipper.right.reduce((sum, n) => sum + cost(n, position), 0)
-
-    sums.push(zipper.sumLeft + zipper.sumRight)
-
-    position++
+  for (let position = 0; position < crabs.length; position++) {
+    sums.push(crabs.reduce((sum, n) => sum + cost(n, position)))
   }
 
   return sums
